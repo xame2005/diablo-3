16b1940fe774d68afaa14c6efaf51cd6cde87164
Samuel Gonzalez <xame2005@gmail.com>
2021-03-27 17:29:26 -0600

    [ADDED] Hero card created (TopHero.vue)

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <BaseLoading v-if="isLoading" />
-    <h1>Profile View</h1>
+    <template v-if="profileData !== null">
+      <MainBlock :profile-data="profileData" />
+    </template>
   </div>
 </template>
 
@@ -9,11 +11,12 @@
 import BaseLoading from '@/components/BaseLoading'
 import setError from '@/mixins/setError'
 import { getApiAccount } from '@/api/search'
+import MainBlock from './MainBlock/Index'
 
 export default {
   name: 'ProfileView',
   mixins: [setError],
-  components: { BaseLoading },
+  components: { BaseLoading, MainBlock },
   data () {
     return {
       isLoading: false,
