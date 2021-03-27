f6d9492fbbaf2ccea012a2a7d941f52220c894f8
Samuel Gonzalez <xame2005@gmail.com>
2021-03-27 01:49:29 -0600

    [CREATED] Spinner component

--- src/main.js
+++ src/main.js
@@ -17,5 +17,15 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  methods: {
+    // Nuestra función
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+    }
+  },
+  // Hook created
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
