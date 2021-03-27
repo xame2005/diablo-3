d11d68e582e85b6ea1b5dd9ea540239475ebe8f2
Samuel Gonzalez <xame2005@gmail.com>
2021-03-27 02:55:03 -0600

    a

---
+++ src/store/modules/loading.js
@@ -0,0 +1,13 @@
+// store/modules/loading.js
+
+export default {
+  namespaced: true,
+  state: {
+    isLoading: false
+  },
+  mutations: {
+    SET_LOADING (state, payload) {
+      state.isLoading = payload
+    }
+  }
+}
