2c9436d36cbfeb097ceec5915788d4d2d2ce80db
Samuel Gonzalez <xame2005@gmail.com>
2021-03-28 17:51:49 -0600

    [CREATED] Lazy Loading for components and keep alive tag implemented for state persistance

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -3,26 +3,76 @@
     <h2 class="font-diablo">Skills</h2>
     <hr class="bg-white" />
 
-    <ActiveSkills :skills="skills.active" />
-    <hr />
-    <PassiveSkills :skills="skills.passive" />
+    <b-nav pills small>
+      <b-nav-item
+        :active="!isPassiveSkillsActive"
+        @click="changeComponent('ActiveSkills')"
+        >Active</b-nav-item
+      >
+      <b-nav-item
+        :active="isPassiveSkillsActive"
+        @click="changeComponent('PassiveSkills')"
+        >Passive</b-nav-item
+      >
+    </b-nav>
+
+    <keep-alive>
+      <component :is="activeComponent" :skills="componentProps" />
+    </keep-alive>
+
+    <!--
+    <ActiveSkills :skills="skills.active"/>
+    <hr>
+    <PassiveSkills :skills="skills.passive"/>
+    -->
   </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
+/* import ActiveSkills from './ActiveSkills'
 import PassiveSkills from './PassiveSkills'
-
+ */
 export default {
   name: 'HeroSkills',
   components: {
-    ActiveSkills,
-    PassiveSkills
+    ActiveSkills: () => import('./ActiveSkills'),
+    PassiveSkills: () => import('./PassiveSkills')
   },
+
+  computed: {
+    /**
+     * Dinamyc props for async dynamic components
+     * @returns {String}
+     */
+    // Con esto estamos generando "props" dinámicas
+    // Si el componente es ActiveSkills pasa como props las activas, si no, las pasivas
+    componentProps () {
+      return this.activeComponent === 'ActiveSkills'
+        ? this.skills.active
+        : this.skills.passive
+    },
+    // Nos dice si el componente "HabilidadesPasivas" está activo o no
+    isPassiveSkillsActive () {
+      return this.activeComponent === 'PassiveSkills'
+    }
+  },
+
+  data () {
+    return {
+      activeComponent: 'ActiveSkills'
+    }
+  },
+
   props: {
     skills: {
       required: true,
       type: Object
+    }
+  },
+
+  methods: {
+    changeComponent (component) {
+      this.activeComponent = component
     }
   }
 }
